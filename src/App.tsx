b46391825713891e748bc1769cf8ea4e4d388a4d
David Silva <davidlsilva.sp@gmail.com>
2024-11-05 09:03:01 -0300

    feat: site basico

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white font-sans">
+      <div className="w-40 h-40 bg-gray-800 rounded-full flex items-center justify-center border-4 border-gray-600 overflow-hidden">
+        <img alt='David Lucas Silva Santos' src="https://avatars.githubusercontent.com/u/29028758?v=4"/>
+      </div>
+      <h1 className="text-2xl mt-4 font-bold">David Silva, Desenvolvedor Front-End</h1>
+      <div className="flex space-x-4 mt-2">
+        <a href="https://www.linkedin.com/in/david-silva-santos/" className="text-blue-400 hover:underline">LinkedIn</a>
+        <a href="https://github.com/delkz" className="text-blue-400 hover:underline">GitHub</a>
+      </div>
     </div>
   );
 }
